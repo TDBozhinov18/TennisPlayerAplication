7e4b8e96191f71556c337a9fdf142dffb7bdafa8
TDBozhinov18 <TDBozhinov18@codingburgas.bg>
2022-11-09 15:25:19 +0200

    Initial commit

---
+++ src/test/java/com/example/tennisplayeraplication/TennisPlayerAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tennisplayeraplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisPlayerAplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
